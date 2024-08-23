7480087d3f66f4040a61b6bf4102e2120948f6ca
seulki <tmfrl4710@gmail.com>
2024-08-24 00:52:38 +0900

    refactor: Payment create, cancel dummy 구현 및 테스트코드 작성

--- src/main/java/com/olivejua/payservice/controller/PaymentController.java
+++ src/main/java/com/olivejua/payservice/controller/PaymentController.java
@@ -4,25 +4,94 @@ import com.olivejua.payservice.controller.request.PaymentCancelRequest;
 import com.olivejua.payservice.controller.request.PaymentCreateRequest;
 import com.olivejua.payservice.controller.response.PaymentCancelResponse;
 import com.olivejua.payservice.controller.response.PaymentCreateResponse;
+import com.olivejua.payservice.domain.User;
+import com.olivejua.payservice.domain.UserLimit;
+import com.olivejua.payservice.error.ApplicationException;
+import com.olivejua.payservice.domain.type.PaymentStatus;
+import com.olivejua.payservice.domain.type.UserStatus;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @RequestMapping("/pay/payments")
 @RestController
 public class PaymentController {
+    private final User dummyUser = User.builder()
+            .id(1L)
+            .name("user1")
+            .email("user1@gmail.com")
+            .status(UserStatus.ACTIVE)
+            .currentBalance(9_500_000L)
+            .accountBank("NH")
+            .accountNumber("123-12-123456-12")
+            .createdAt(LocalDateTime.of(2024, 8, 31, 12, 0, 0))
+            .updatedAt(LocalDateTime.of(2024, 9, 1, 12, 0, 0))
+            .build();
+
+    private final UserLimit dummyUserLimit = UserLimit.builder()
+            .id(1L)
+            .user(dummyUser)
+            .maxBalance(10_000_000L)
+            .singlePaymentLimit(1_000_000L)
+            .dailyPaymentLimit(3_000_000L)
+            .monthlyPaymentLimit(7_000_000L)
+            .build();
+
+    private final long todayTransactionAmount = 2_100_000;
+    private final long thisMonthTransactionAmount = 6_300_000;
 
     @PostMapping
     ResponseEntity<PaymentCreateResponse> createPayment(@RequestBody PaymentCreateRequest request) {
+        //FIXME 비즈니스 로직 구현 후 제거하기
+        if (!request.userId().equals(dummyUser.getId())) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, "USER_NOT_FOUND_OR_WITHDRAWN", "User does not exist or is in a withdrawn state.");
+        }
 
+        if (dummyUserLimit.getSinglePaymentLimit() < request.amount()) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, "SINGLE_PAYMENT_LIMIT_EXCEEDED", "Single payment limit exceeded.");
+        }
+
+        if (dummyUserLimit.getDailyPaymentLimit() < todayTransactionAmount + request.amount()) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, "DAILY_LIMIT_EXCEEDED", "Daily payment limit exceeded.");
+        }
+
+        if (dummyUserLimit.getMonthlyPaymentLimit() < thisMonthTransactionAmount + request.amount()) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, "MONTHLY_LIMIT_EXCEEDED", "Monthly payment limit exceeded.");
+        }
+
+        if (dummyUserLimit.getMaxBalance() < dummyUser.getCurrentBalance() + request.amount()) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, "MAX_BALANCE_EXCEEDED", "Payment amount exceeds the user's maximum allowed balance.");
+
+        }
+
+        PaymentCreateResponse response = new PaymentCreateResponse(1L, request.userId(), request.amount(), "NH", "123-12-123456-12", UUID.randomUUID().toString(), LocalDateTime.now(), PaymentStatus.DONE, LocalDateTime.now());
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(null);
+                .body(response);
     }
 
     @PostMapping("/{paymentId}/cancel")
-    ResponseEntity<PaymentCancelResponse> cancelPayment(@PathVariable Long paymentId, @RequestBody PaymentCancelRequest request) {
+    ResponseEntity<PaymentCancelResponse> cancelPayment(@PathVariable("paymentId") Long paymentId, @RequestBody PaymentCancelRequest request) {
+        //FIXME 비즈니스 로직 구현 후 제거하기
+        if (request.userId().equals(2L)) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, "USER_NOT_FOUND_OR_WITHDRAWN", "User does not exist or is in a withdrawn state.");
+        }
+
+        if (request.userId().equals(3L)) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, "UNAUTHORIZED_CANCELLATION", "The requester is not authorized to cancel this payment.");
+        }
+
+        if (paymentId.equals(2L)) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, "PAYMENT_NOT_FOUND", "Payment information not found.");
+        }
+
+        if (paymentId.equals(3L)) {
+            throw new ApplicationException(HttpStatus.BAD_REQUEST, "ALREADY_CANCELED", "The payment has already been canceled.");
+        }
 
         return ResponseEntity
                 .status(HttpStatus.OK)
--- src/test/java/com/olivejua/payservice/controller/PaymentControllerCancelTest.java
+++ src/test/java/com/olivejua/payservice/controller/PaymentControllerCancelTest.java
@@ -1,87 +1,156 @@
 package com.olivejua.payservice.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.olivejua.payservice.controller.request.PaymentCancelRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.nio.charset.StandardCharsets;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
 class PaymentControllerCancelTest {
+    @Autowired
+    private MockMvc mockMvc;
+    private ObjectMapper objectMapper;
 
-    @Test
-    void 유저가_활성상태가_아니라면_결제에_실패한다() {
-        //given
-
-        //when
-
-        //then
+    @BeforeEach
+    void setUp() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        this.objectMapper = objectMapper;
     }
 
     @Test
-    void 결제_요청정보가_결제에_실패한다() {
+    void 유저가_활성상태가_아니라면_결제취소에_실패한다() throws Exception {
         //given
+        PaymentCancelRequest request = new PaymentCancelRequest(2L);
 
-        //when
-
-        //then
+        //when & then
+        mockMvc.perform(post("/pay/payments/{paymentId}/cancel", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("USER_NOT_FOUND_OR_WITHDRAWN"))
+                .andExpect(jsonPath("$.message").value("User does not exist or is in a withdrawn state."));
     }
 
     @Test
-    void 유저의_현재_보유금액과_결제금액_합산금액이_최대한도금액을_초과한다면_결제에_실패한다() {
+    void 결제정보가_존재하지_않으면_결제취소에_실패한다() throws Exception {
         //given
+        PaymentCancelRequest request = new PaymentCancelRequest(1L);
 
-        //when
-
-        //then
+        //when & then
+        mockMvc.perform(post("/pay/payments/{paymentId}/cancel", 2L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("PAYMENT_NOT_FOUND"))
+                .andExpect(jsonPath("$.message").value("Payment information not found."));
     }
 
     @Test
-    void 유저의_머니결제금액이_요청월_기준_1달_결제최대한도금액을_초과하면_결제에_실패한다() {
+    void 유저가_결제자가_아닌_경우_결제취소에_실패한다() throws Exception {
         //given
+        PaymentCancelRequest request = new PaymentCancelRequest(3L);
 
-        //when
-
-        //then
+        //when & then
+        mockMvc.perform(post("/pay/payments/{paymentId}/cancel", 2L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("UNAUTHORIZED_CANCELLATION"))
+                .andExpect(jsonPath("$.message").value("The requester is not authorized to cancel this payment."));
     }
 
     @Test
-    void 유저의_머니결제금액이_요청일자_기준_1일_결제최대한도금액을_초과하면_결제에_실패한다() {
+    void 요청결제취소건의_상태가_이미_취소일_경우_결제에_실패한다() throws Exception {
         //given
+        PaymentCancelRequest request = new PaymentCancelRequest(1L);
 
-        //when
+        //when & then
+        mockMvc.perform(post("/pay/payments/{paymentId}/cancel", 3L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("ALREADY_CANCELED"))
+                .andExpect(jsonPath("$.message").value("The payment has already been canceled."));
+    }
 
-        //then
+    @Disabled
+    @Test
+    void 결제대행사에서_실패응답이_오면_결제에_실패한다() throws Exception {
+        //given
+        PaymentCancelRequest request = new PaymentCancelRequest(1L);
+
+        //when & then
+        mockMvc.perform(post("/pay/payments/{paymentId}/cancel", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("PAYMENT_GATEWAY_FAILURE")) //TODO ErrorCode 재정의하기
+                .andExpect(jsonPath("$.message").value("Payment processing failed due to external payment gateway error."));
+    }
+
+    @Disabled // TODO given에서 sleep 조건 걸기
+    @Test
+    void 처리시간이_5초_초과되면_결제에_실패한다() throws Exception {
+        //given
+        PaymentCancelRequest request = new PaymentCancelRequest(1L);
+
+        //when & then
+        mockMvc.perform(post("/pay/payments/{paymentId}/cancel", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("PAYMENT_TIMEOUT"))
+                .andExpect(jsonPath("$.message").value("Payment processing time exceeded the limit of 5 seconds."));
     }
 
     @Test
-    void 유저의_머니결제금액이1회_결제최대한도금액을_초과하면_결제에_실패한다() {
+    void 모든_조건을_충족하면_결제성공_후_유저_보유금액이_머니_결제금액만큼_업데이트된다() throws Exception {
         //given
+        PaymentCancelRequest request = new PaymentCancelRequest(1L);
 
-        //when
-
-        //then
-    }
-
-    @Test
-    void 결제대행사에서_실패응답이_오면_결제에_실패한다() {
-        //given
-
-        //when
-
-        //then
-    }
-
-    @Test
-    void 처리시간이_5초_초과되면_결제에_실패한다() {
-        //given
-
-        //when
-
-        //then
-    }
-
-    @Test
-    void 모든_조건을_충족하면_결제성공_후_유저_보유금액이_머니_결제금액만큼_업데이트된다() {
-        //given
-
-        //when
-
-        //then
+        //when & then
+        mockMvc.perform(post("/pay/payments/{paymentId}/cancel", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
