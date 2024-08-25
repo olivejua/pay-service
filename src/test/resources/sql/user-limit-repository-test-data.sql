50421c672416bc56ba132f0e0f68fe101532d654
seulki <tmfrl4710@gmail.com>
2024-08-25 23:32:53 +0900

    test: test-data sql 파일 생성

---
+++ src/test/resources/sql/user-limit-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into user_limits (user_id, max_balance, single_payment_limit, daily_payment_limit, monthly_payment_limit, created_at, updated_at)
+values (1, 10000000, 2000000, 3000000, 7000000, '2024-08-31 12:00:00', '2024-09-01 12:00:00');
---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into users (name, email, status, current_balance, account_bank, account_number, created_at, updated_at)
+values ('user1', 'user1@gmail.com', 'ACTIVE', '9500000', 'NH', '123-12-123456-12', '2024-08-31 12:00:00', '2024-09-01 12:00:00');
