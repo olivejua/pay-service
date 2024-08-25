f218205c7e5ddaf6607936d4c4336ff0d483102d
seulki <tmfrl4710@gmail.com>
2024-08-25 19:27:25 +0900

    refactor: 결제취소 Controller 로직 서비스로 이동

--- src/main/java/com/olivejua/payservice/controller/PaymentController.java
+++ src/main/java/com/olivejua/payservice/controller/PaymentController.java
@@ -4,7 +4,6 @@ import com.olivejua.payservice.controller.request.PaymentCancelRequest;
 import com.olivejua.payservice.controller.request.PaymentCreateRequest;
 import com.olivejua.payservice.controller.response.PaymentCancelResponse;
 import com.olivejua.payservice.controller.response.PaymentCreateResponse;
-import com.olivejua.payservice.error.ApplicationException;
 import com.olivejua.payservice.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,7 +18,7 @@ public class PaymentController {
 
     @PostMapping
     ResponseEntity<PaymentCreateResponse> createPayment(@RequestBody PaymentCreateRequest request) {
-        PaymentCreateResponse response = paymentService.createPayment(request);
+        final PaymentCreateResponse response = paymentService.createPayment(request);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -28,25 +27,10 @@ public class PaymentController {
 
     @PostMapping("/{paymentId}/cancel")
     ResponseEntity<PaymentCancelResponse> cancelPayment(@PathVariable("paymentId") Long paymentId, @RequestBody PaymentCancelRequest request) {
-        //FIXME 비즈니스 로직 구현 후 제거하기
-        if (request.userId().equals(2L)) {
-            throw new ApplicationException(HttpStatus.BAD_REQUEST, "USER_NOT_FOUND_OR_WITHDRAWN", "User does not exist or is in a withdrawn state.");
-        }
-
-        if (request.userId().equals(3L)) {
-            throw new ApplicationException(HttpStatus.BAD_REQUEST, "UNAUTHORIZED_CANCELLATION", "The requester is not authorized to cancel this payment.");
-        }
-
-        if (paymentId.equals(2L)) {
-            throw new ApplicationException(HttpStatus.BAD_REQUEST, "PAYMENT_NOT_FOUND", "Payment information not found.");
-        }
-
-        if (paymentId.equals(3L)) {
-            throw new ApplicationException(HttpStatus.BAD_REQUEST, "ALREADY_CANCELED", "The payment has already been canceled.");
-        }
+        final PaymentCancelResponse response = paymentService.cancelPayment(paymentId, request);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(null);
+                .body(response);
     }
 }
