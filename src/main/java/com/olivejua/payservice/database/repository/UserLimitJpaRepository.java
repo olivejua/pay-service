6490ee3817ba8987b4d755e0758b27e10dcb3d08
seulki <tmfrl4710@gmail.com>
2024-08-25 23:32:40 +0900

    fix: UserLimitJparepository Param 애노테이션 추가

--- src/main/java/com/olivejua/payservice/database/repository/UserLimitJpaRepository.java
+++ src/main/java/com/olivejua/payservice/database/repository/UserLimitJpaRepository.java
@@ -3,6 +3,7 @@ package com.olivejua.payservice.database.repository;
 import com.olivejua.payservice.database.entity.UserLimitEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -10,5 +11,5 @@ public interface UserLimitJpaRepository extends JpaRepository<UserLimitEntity, L
     @Query("select userLimit " +
             "from UserLimitEntity userLimit " +
             "where userLimit.user.id = :userId")
-    Optional<UserLimitEntity> findByUserId(Long userId);
+    Optional<UserLimitEntity> findByUserId(@Param("userId") Long userId);
 }
