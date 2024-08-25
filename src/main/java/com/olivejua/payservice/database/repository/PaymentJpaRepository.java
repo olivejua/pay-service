fca27e0ac72d1d7d88f423ccc61c3316f5d0ca2f
seulki <tmfrl4710@gmail.com>
2024-08-26 00:08:19 +0900

    feat: PaymentJpaRepository 기간 합산금액 쿼리 추가

--- src/main/java/com/olivejua/payservice/database/repository/PaymentJpaRepository.java
+++ src/main/java/com/olivejua/payservice/database/repository/PaymentJpaRepository.java
@@ -1,7 +1,27 @@
 package com.olivejua.payservice.database.repository;
 
 import com.olivejua.payservice.database.entity.PaymentEntity;
+import com.olivejua.payservice.domain.type.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 
 public interface PaymentJpaRepository extends JpaRepository<PaymentEntity, Long> {
+
+    // TODO 오늘, 이번달 거래한 결제금액 계산을 DB연산으로 할지 다 가져와서 연산할지 고민하기
+    // TODO 비교할 컬럼을 생성일자로 해야할까?
+    // TODO 날짜 계산도 빅데이터가 넘어가면 날짜에 컬럼을 추가해서 하는 것을 고려한다
+    @Query("SELECT COALESCE(SUM(payment.amount), 0) " +
+            "FROM PaymentEntity payment " +
+            "WHERE payment.user.id = :userId " +
+            "AND payment.status = :status " +
+            "AND payment.createdAt >= :startOfDay " +
+            "AND payment.createdAt <= :endOfDay")
+    long findTotalAmountByUserIdAndStatusAndCreatedAtBetween(
+            @Param("userId") Long userId,
+            @Param("status") PaymentStatus status,
+            @Param("startOfDay") LocalDateTime startOfDay,
+            @Param("endOfDay") LocalDateTime endOfDay);
 }
