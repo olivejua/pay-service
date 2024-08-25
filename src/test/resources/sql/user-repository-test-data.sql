e98ed2db28ce0793d243e2de85168c57664e84db
seulki <tmfrl4710@gmail.com>
2024-08-26 00:08:50 +0900

    test: user-repository-test-data.sql user2 test data 추가

--- src/test/resources/sql/user-repository-test-data.sql
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -1,2 +1,3 @@
 insert into users (name, email, status, current_balance, account_bank, account_number, created_at, updated_at)
-values ('user1', 'user1@gmail.com', 'ACTIVE', '9500000', 'NH', '123-12-123456-12', '2024-08-31 12:00:00', '2024-09-01 12:00:00');
+values ('user1', 'user1@gmail.com', 'ACTIVE', '9500000', 'NH', '123-12-123456-12', '2024-07-31 12:00:00', '2024-08-01 12:00:00'),
+       ('user2', 'user2@gmail.com', 'WITHDRAWN', '0', 'NH', '234-23-234567-23', '2024-08-01 12:00:00', '2024-08-10 12:00:00');
